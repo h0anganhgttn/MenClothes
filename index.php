1d3ac578a7f3448284e8b6d1b7a96bec63eaf877
Hoang Anh <killerstevena@gmail.com>
2020-01-15 14:08:44 +0700

    MenClothes

---
+++ index.php
@@ -0,0 +1,324 @@
+<?php
+include("template/header.php");
+?>
+
+
+
+
+<main>
+	<section>
+		<div class="container" style="margin-top: 2em;">
+			<div class="row row-reverse-xs" style="background-color: white;">
+				<div class="col-12 col-lg-3 order-2 order-lg-1 " style="margin-bottom: 2em;">
+					<div id="menu"><b>Product Menu</b></div>
+					<div id="dropdown" style="padding-top: 10px;">
+						<ul>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> T-Shirts</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Sweaters</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Sweatshirts</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Button Down Shirts</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Jackets & Coats</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Pants & Shorts</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Tracksuits</a></li>
+							<div class="dropdown-divider"></div>
+							<li><a href="product.php"><i class="fas fa-arrow-circle-right"></i> Underwear</a></li>
+
+						</ul>
+					</div>
+				</div>
+				<div class="col-12 col-lg-9 order-1 order-lg-2" >
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="https://imagena1.lacoste.com/dw/image/v2/AAUP_PRD/on/demandware.static/-/Sites-master/default/dwe36dd156/TH8128_258_20.jpg?imwidth=914&impolicy=product" class="d-block w-100" class="mySlides" alt="...">
+							</div>
+							<div class="carousel-item">
+								<img src="https://imagena1.lacoste.com/dw/image/v2/AAUP_PRD/on/demandware.static/-/Sites-master/default/dw640e3780/TH6386_031_20.jpg?imwidth=914&impolicy=product" width="50%" height="50%" class="d-block w-100" class="mySlides" alt="...">
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--end container-->
+
+
+
+
+		<!-------------------------- Product-------------------------->
+		<div class="container">
+			<div class="brick">
+				<h5 class="title-myclass">T-Shirts</h5>
+			</div>
+			<div class="product">
+				<div class="tab-content" id="myTabContent">
+					<div class="tab-pane fade show active" id="tab1">
+						<div class="owl-carousel owl-theme owl-carousel-myclass1">
+							<div class="item">
+								<div class="cart">
+									<a href="detail.php">
+										<img class="cart-img" src="Assets/images/T-shirt1.png">
+									</a>
+									<div class="cart-title">Men's Striped-Crewneck T-Shirt</div>
+									<div class="price">
+										<span class="price-New">New: $35.99</span>
+										<span class="price-old">Old: $60.00</span>
+									</div>
+									<div class="link-cart">
+										<a href="#">
+											<i class="fa fa-shopping-cart"></i> Add to cart
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="cart">
+									<a href="detail.php">
+										<img class="cart-img" src="Assets/images/T-shirt2.png">
+									</a>
+									<div class="cart-title">Men's Graphic Croc T-shirt</div>
+									<div class="price">
+										<span class="price-New">New: $35.99</span>
+										<span class="price-old">Old: $60.00</span>
+									</div>
+									<div class="link-cart">
+										<a href="#">
+											<i class="fa fa-shopping-cart"></i> Add to cart
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="cart">
+									<a href="detail.php">
+										<img class="cart-img" src="Assets/images/T-shirt3.png">
+									</a>
+									<div class="cart-title">Men's Piqué Crewneck T-Shirt</div>
+									<div class="price">
+										<span class="price-New">New: $50.99</span>
+										<span class="price-old">Old: $85.00</span>
+									</div>
+									<div class="link-cart">
+										<a href="#">
+											<i class="fa fa-shopping-cart"></i> Add to cart
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="cart">
+									<a href="detail.php">
+										<img class="cart-img" src="Assets/images/T-shirt4.png">
+									</a>
+									<div class="cart-title">Men's Crewneck Checkered T-Shirt</div>
+									<div class="price">
+										<span class="price-New">New: $35.99</span>
+										<span class="price-old">Old: $60.00</span>
+									</div>
+									<div class="link-cart">
+										<a href="#">
+											<i class="fa fa-shopping-cart"></i> Add to cart
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="container">
+					<div class="brick">
+						<h5 class="title-myclass">Sweaters</h5>
+					</div>
+					<div class="product">
+						<div class="tab-content" id="myTabContent">
+							<div class="tab-pane fade show active" id="tab4">
+								<div class="owl-carousel owl-theme owl-carousel-myclass1">
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweater1.png">
+											</a>
+											<div class="cart-title">Men's V-neck Caviar Piqué Sweater</div>
+											<div class="price">
+												<span class="price-New">New: $35.99</span>
+												<span class="price-old">Old: $60.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweater2.png">
+											</a>
+											<div class="cart-title">Men's Wool And Alpaca Sweater</div>
+											<div class="price">
+												<span class="price-New">New: $114.99</span>
+												<span class="price-old">Old: $230.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweater3.png">
+											</a>
+											<div class="cart-title">Men's Cotton Blend Sweater</div>
+											<div class="price">
+												<span class="price-New">New: $91.99</span>
+												<span class="price-old">Old: $185.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweater4.png">
+											</a>
+											<div class="cart-title">Men's 3D-Lettering Sweater</div>
+											<div class="price">
+												<span class="price-New">New: $94.99</span>
+												<span class="price-old">Old: $190.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="brick">
+						<h5 class="title-myclass">Sweatshirts</h5>
+					</div>
+					<div class="product">
+						<div class="tab-content" id="myTabContent">
+							<div class="tab-pane fade show active" id="tab4">
+								<div class="owl-carousel owl-theme owl-carousel-myclass1">
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweatshirt1.png">
+											</a>
+											<div class="cart-title">Men's SPORT Tennis Fleece Zip Up</div>
+											<div class="price">
+												<span class="price-New">New: $48.99</span>
+												<span class="price-old">Old: $98.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweatshirt2.png">
+											</a>
+											<div class="cart-title">Men's Hooded Fleece Sweatshirt</div>
+											<div class="price">
+												<span class="price-New">New: $93.99</span>
+												<span class="price-old">Old: $135.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweatshirt3.png">
+											</a>
+											<div class="cart-title">Men's Standup-Collar Sweatshirt</div>
+											<div class="price">
+												<span class="price-New">New: $67.99</span>
+												<span class="price-old">Old: $98.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+
+									<div class="item">
+										<div class="cart">
+											<a href="detail.php">
+												<img class="cart-img" src="assets/images/Sweatshirt4.png">
+											</a>
+											<div class="cart-title">Men's Zip Fleece Sweatshirt</div>
+											<div class="price">
+												<span class="price-New">New: $98.00</span>
+												<span class="price-old">Old: $145.00</span>
+											</div>
+											<div class="link-cart">
+												<a href="#">
+													<i class="fa fa-shopping-cart"></i> Add to cart
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--------------------------end Product-------------------------->
+
+</main>
+<!--end main-->
+
+
+
+<?php
+include("template/footer.php");
+?>
